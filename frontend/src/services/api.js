8bfbd9acc271dd27a07066cf1c2711f73d2c0988
Altaf <chmcs2026altaf.shaikh@gmail.com>
2026-03-07 20:00:09 +0530

    updated Project

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://book-my-hotel-backend-ta2n.onrender.com/api"
+  baseURL: "https://book-my-hotel-backend-ta2n.onrender.com/api/hotels"
 });
 
 export default API;
